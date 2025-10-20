4a86ad0ab347ad6cef0335a121002e2f979a99b8
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2025-10-20 13:48:41 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ import fastifyStatic from "@fastify/static";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 const server = fastify();
+
 
 // Serve arquivos estáticos da pasta 'public'
 await server.register(fastifyStatic, {
@@ -16,22 +19,28 @@ await server.register(fastifyStatic, {
   prefix: "/", // opcional
 });
 
+
 // Configura CORS
 await server.register(cors, {
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
+
 const database = new DatabasePostegres();
+
 
 // --- ROTAS ---
 server.post("/videos", async (request, reply) => {
   const { title, description, duration } = request.body;
 
+
   await database.create({ title, description, duration });
+
 
   return reply.status(201).send();
 });
+
 
 server.get("/videos", async (request) => {
   const search = request.query.search;
@@ -39,21 +48,31 @@ server.get("/videos", async (request) => {
   return videos; // sempre retorna array
 });
 
+
 server.put("/videos/:id", async (request, reply) => {
   const videoId = request.params.id;
   const { title, description, duration } = request.body;
 
+
   await database.update(videoId, { title, description, duration });
+
 
   return reply.status(204).send();
 });
 
-server.delete("/videos/:id", async(request, reply) => {
-    const videoId = request.params.id;
-    await database.delete(videoId);
-    return reply.status(204).send();
+
+server.delete("/videos/:id", async (request, reply) => {
+  const videoId = request.params.id;
+
+
+  await database.delete(videoId);
+
+
+  return reply.status(204).send();
 });
 
+
 server.listen({
-    port: process.env.PORT ?? 3333,
+  port: process.env.PORT ?? 3333,
 }).then(() => console.log("Servidor rodando em http://localhost:3333"));
+
